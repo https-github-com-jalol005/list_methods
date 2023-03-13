4352402e067c3c2be55f0685f8e8e6f01e4bc724
Mirjalol <jalolubaydullayev575@gmail.com>
2023-03-13 12:05:39 +0500

    07

--- bin/list_method07.dart
+++ bin/list_method07.dart
@@ -7,4 +7,17 @@
         int: return answer
 */
 
-void main() {}
+int func(List list01){
+  int i = 0;
+  int a = 0;
+  while (i < list01.length){
+    if (list01[i] == 0){
+      a ++;
+    }
+    i ++;
+  }
+  return a;
+}
+void main() {
+  print(func([1,0,1,1,0,0,0]));
+}
