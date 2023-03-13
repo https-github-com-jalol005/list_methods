6be9fac15353c55185a0983b5ae9284f620cdf44
Mirjalol <jalolubaydullayev575@gmail.com>
2023-03-13 12:12:43 +0500

    10

--- bin/list_method10.dart
+++ bin/list_method10.dart
@@ -7,4 +7,21 @@
         list: return answer
 */
 
-void main() {}
+   List func(List list1){
+  int i = 0;
+  int a = 0;
+  int b = 0;
+  while (i < list1.length){
+    if (list1[i] == 1){
+      a ++;
+    }
+    else {
+      b ++;
+    }
+    i ++;
+  }
+  return [a] + [b];
+}
+void main() {
+  print(func([1,0,1,1,0,0,0,1]));
+}
